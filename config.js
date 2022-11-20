c343d2d9e38072303e11ce52b474155ae09b1c61
JasmineGHub <103321703+JasmineGHub@users.noreply.github.com>
2022-11-20 23:55:04 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Vkli xink';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100075387319507';
+const eventName = 'Món quà nhảm nhí tới';
+const titleCard = 'Iu zl';
+const contentCard = 'Chúc mừng sinh nhật em bé Ling siu siu siu thơm cụa anhhhh. Yêu công chúa nhiều nhất vũ trụ ngân hà bao na hi :3';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
